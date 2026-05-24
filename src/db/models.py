bcd7a5a5c79551aea5440c16516281709b356ab7
lochanjangid <lochanjangidcoder@gmail.com>
2026-05-25 01:23:03 +0530

    feat: fix and upgrade db functionalitites

--- src/db/models.py
+++ src/db/models.py
@@ -1,5 +1,5 @@
-from db.connection import get_db
-
+from src.db.connection import get_db
+from typing import Dict
 
 def login_player(name: str):
     db = get_db()
@@ -9,18 +9,31 @@ def login_player(name: str):
         row = db.query("SELECT last_insert_rowid()", operation="fetchone")
     return row[0]
 
-def save_score(player_id: int, game_id: int, duration: int, earned_coins: int):
+def save_score(player_id: int, game_id: int, duration: float, earned_coins: int):
     db = get_db()
     db.query("INSERT INTO sessions (player_id, game_id, duration, earned_coins) VALUES (?, ?, ?, ?)", (player_id, game_id, duration, earned_coins))
+
+def insert_games(games: Dict[int, str]):
+    db = get_db()
+    for k, v in games.items():
+        db.query("""
+        INSERT INTO games (id, name)
+        VALUES (?, ?)
+        ON CONFLICT(id) DO NOTHING;
+            """, (k, v))
 
 def get_leaderboard():
     db = get_db()
     rows = db.query("""
-        SELECT players.name, SUM(earned_coins) AS coins
-          FROM sessions 
-          JOIN players ON player_id=players.id
-        GROUP BY player_id 
-        ORDER BY coins
-        LIMIT 3;""", operation="fetchall")
+        SELECT *, 
+               DENSE_RANK() OVER(ORDER BY coins) AS `rank` 
+          FROM (
+            SELECT players.name, 
+                   SUM(earned_coins) AS coins
+            FROM sessions 
+            JOIN players ON sessions.player_id=players.id
+            GROUP BY player_id 
+        ) AS tb;
+        """, operation="fetchall")
     return rows
     
