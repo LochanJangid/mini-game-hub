8d3317b836aa89323f18102e00696216652c77f1
lochanjangid <lochanjangidcoder@gmail.com>
2026-05-25 01:25:06 +0530

    feat: add execute_setup function that must run in the first of this game hub.

---
+++ src/first_run.py
@@ -0,0 +1,9 @@
+from src.db.create_tables import create_tables
+from src.db.models import insert_games
+from typing import Dict
+
+
+def execute_setup(games: Dict[int, str]):
+    create_tables()
+    insert_games(games)
+    
