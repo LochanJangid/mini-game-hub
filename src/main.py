7f700e869e47d9e377f73da77fb2f26790660a13
lochanjangid <lochanjangidcoder@gmail.com>
2026-05-31 16:40:37 +0530

    feat: add leaderboard to command and make its ui as class

--- src/main.py
+++ src/main.py
@@ -5,22 +5,22 @@ from src.first_run import execute_setup
 from src.games.guessing_game import GuessingGame
 from src.games.tic_tac_toe import ToeGame
 from src.games.typing_game import TypingGame
-from src.ui.main_ui import MainUI
+from src.ui.main_ui import MainUI, show_leaderboard
 
 MAP = {
     "Typing Speed Test": TypingGame,
     "Guess Number Game": GuessingGame,
     "Tic Tac Toe": ToeGame,
+    "Leaderboard": show_leaderboard
 }
 
-GAMES = {0: "Typing Speed Test", 1: "Guess Number Game", 2: "Tic Tac Toe"}
+GAMES = {0: "Typing Speed Test", 1: "Guess Number Game", 2: "Tic Tac Toe", 9: "Leaderboard"}
 
 
 def main():
     execute_setup(GAMES)
     ui = MainUI()
     ui.intro("👾 Mini Game Hub 🎮", "Your central hub for Python mini-games 🎮.")
-    ui.show_leaderboard()
     player = login_player(ui.prompt_player_name())
     print("Welcome", player)
     ui.show_commands(GAMES)
@@ -29,7 +29,6 @@ def main():
             user_command = int(ui.prompt_command())
             game = MAP[GAMES[user_command]]()
             game.run(player)
-            break
         except (KeyError, ValueError):
             print("❌ Invalid command! Commands are:")
             ui.show_commands(GAMES)
