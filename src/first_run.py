e9cf1400c815c47ba32ba68b6bccb1916fdf0331
lochanjangid <lochanjangidcoder@gmail.com>
2026-06-01 17:22:07 +0530

    fix: add to insert games except leaderboard

--- src/first_run.py
+++ src/first_run.py
@@ -2,8 +2,8 @@ from src.db.create_tables import create_tables
 from src.db.models import insert_games
 from typing import Dict
 
-
 def execute_setup(games: Dict[int, str]):
+    local_games = games.copy()
     create_tables()
-    insert_games(games)
-        
+    local_games.pop(9, None)
+    insert_games(local_games)
